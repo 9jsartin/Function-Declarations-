039d7af89efec8701cfe0dab1c8e80a621d88309
9jsartin <42806630+9jsartin@users.noreply.github.com>
2018-09-26 14:03:27 -0500

    3rd task

---
+++ Function-Declarations--master/Function-Declarations-master/f1.js
@@ -0,0 +1,125 @@
+/*
+** This is the file f1.js
+** Read each comment below, declare each function.
+** These are all PURE FUNCTIONS, they only depend and change the input values.
+*/
+
+
+/*******************************NUMBER FOCUSED****************************/
+
+// (1) First Task
+// Declare a function named double() which outputs a number double the input number.
+function double (a){
+    let total = a*a 
+    return total 
+}
+
+
+
+
+
+
+// (2) Second Task
+// Declare a function named plusplus() which outputs a number one higher than the input number.
+function plusplus (g){
+    let total= g+1
+    return total 
+}
+
+
+
+
+
+
+
+// (3) Third Task
+// Declare a function named oppRecip() which outputs the opposite reciprocal of the input number.
+function oppRecip()
+
+
+
+
+
+
+// (4) Fourth Task
+// Declare a function named pthagorean() which has two inputs you guessed it a and b.
+// The function outputs the value of the third side of the right triangle.
+
+
+
+
+
+
+
+// (5) Fifth Task
+// Declare a function named circleArea() which has one input named radius.
+// The function outputs the area of the circle.
+
+
+
+
+
+
+
+// (6) Sixth Task
+// Declare a function named boxVolume() which has three inputs: length, width, and depth.
+// The function outputs the volume of the box;
+
+
+
+
+
+
+
+/**************************** STRING FOCUSED *********************************/
+
+
+
+// (7) Seventh Task
+// Declare a function named thisIsMadness() which accepts one string input named city.
+// The function should output the string: "No! This is <city>!!"
+
+
+
+
+
+
+
+// (8) Eigth Task
+// Declare a function named yo() which accepts one string input named fam.
+// The function should output the string: "yo <fam>!  What's good <fam>??"
+
+
+
+
+
+
+
+// (9) Ninth Task
+// Declare a function named beefDay() which has no inputs.
+// This function should output a long string, IN ALL CAPS, of "BEEF DAY" with lots of extra E's, F's, A's, and Y's
+
+
+
+
+
+
+
+
+/**************************** BOOLEAN FOCUSED *******************************/
+
+
+// (10) Tenth Task
+// Declare a function named xor() which has two inputs a and b.
+// This function returns the exclusive or of the two boolean inputs.
+
+
+
+
+
+
+
+
+
+
+/**************************** END OF FILE *******************************/
